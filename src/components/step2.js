0d179346b17fd0eeb22e878ec81bbe2be0c95168
Supriya Khandekar <supriyaskhandekar@gmail.com>
2018-03-26 17:08:26 +0800

    Added template form

--- src/components/step2.js
+++ src/components/step2.js
@@ -3,11 +3,49 @@ import './step2.css';
 import './step_shared.css';
 
 
+class TemplateForm extends Component {
+
+render() {
+  return (
+      <div class='form-box'>
+        <div class='form-questions'>
+          <div class='form-header'>Create a Template.</div>
+
+          <p class='question-format'>Write your Introductory Pitch.</p>
+          <textarea class='question-textarea'>Summer Analyst</textarea>
+          <p class='question-format'>What position are you applying for?</p>
+          <textarea class='question-textarea'>I am a third year student at Yale-NUS College, majoring in
+          Economics and minoring in Arts and Humanities. Having experience in both the technical and theoretical
+          space, I feel that I would contribute great work to your company</textarea>
+          <p class='question-format'>Talk about your experience...</p>
+          <textarea class='question-textarea'>Two summers ago, I interned at Goldman Sachs and last year,
+          I worked at Oliver Wyman...</textarea>
+          <p class='question-format'>Concluding Remarks?</p>
+          <textarea class='question-textarea'>I would greatly value an opportunity to work at your company.
+          I truly hope that you will consider my application.</textarea>
+          <button class='submit-button' onClick={this.props.closePopup} >SUBMIT</button>
+        </div>
+
+      </div>
+  )
+}
+}
+
 class Step2 extends Component {
+
+  //code for popup inspired from : https://codepen.io/bastianalbers/pen/PWBYvz?editors=0010
 
   constructor(props) {
     super(props);
-    this.state = { /* initial state */ };
+    this.state = {
+      popupDisplayed :false
+    };
+  }
+
+  togglePopup() {
+    this.setState({
+      popupDisplayed: !this.state.popupDisplayed
+    });
   }
 
   render() {
@@ -50,9 +88,8 @@ class Step2 extends Component {
             </div>
             <div class='template_body'>Create a New Template:</div>
             <div class='template_container'>
-              <a href=''>
-                <div class='plus_button'> </div>
-              </a>
+            <div class='plus_button' onClick = {this.togglePopup.bind(this)}> </div>
+            {this.state.popupDisplayed ? <TemplateForm closePopup={this.togglePopup.bind(this)}/> : null}
             </div>
           </div>
       </div>
