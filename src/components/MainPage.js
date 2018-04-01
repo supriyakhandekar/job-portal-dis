7daf16fe006b7273ebeb5ec73ce36664082bb4a9
Supriya Khandekar <supriyaskhandekar@gmail.com>
2018-04-01 20:37:10 +0800

    Fixed stock photos and fonts

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -60,7 +60,7 @@ class MainPage extends Component {
     return (
       <div>
       <div class='inital_page_wrapper'>
-        <div class='inital_description'>
+        <div id='title'>
           <div>CT2C</div>
           <p class='company-motto'>Cut to the chase</p>
           <a href='#section2'>
@@ -68,15 +68,15 @@ class MainPage extends Component {
           </a>
         </div>
         <div class='inital_description'>
-          <p class='step'>Step 1</p>
+          <p class='step'>1.</p>
           <p class='step_description'>
           Search for companies that interest you. Add them to your 'shopping bag' if you like what you see!
           </p>
-          <p class='step'>Step 2</p>
+          <p class='step'>2.</p>
           <p class='step_description'>
           Create a personalized template and let us fill in the company-specific information for every company you find interesting!
           </p>
-          <p class='step'>Step 3</p>
+          <p class='step'>3.</p>
           <p class='step_description'>
             Let us find you the emails of the important people so you can send away!
           </p>
